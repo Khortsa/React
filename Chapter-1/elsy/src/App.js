afa6d4f3232f4b95e3ae47f21c0f1b299eb09e68
Tenzin Choezom <khortsa2021@gmail.com>
2022-03-01 15:23:45 +0100

    create elsy 2

--- Chapter-1/elsy/src/App.js
+++ Chapter-1/elsy/src/App.js
@@ -1,31 +1,111 @@
+// import React from "react";
+// import './App.css';
+// import { Box} from "./components/Box";
+// import './styles/global.css';
+
+
+
+
+// function App() {
+//     const tempMin = -20;
+//     const tempMax = 40;
+//     const heartMin = 80;
+//     const heartMax = 180;
+//     const stepsMin = 0;
+//     const stepsMax = 50000;
+
+//     return (
+//       <div class = 'container-fluid'>
+//         <div class = 'row'>
+//          <Box icon='local_drink' color='#3A85FF' value='1.5' unit='L'/>
+//          <Box icon='directions_walk' color='black' value='3000' unit='steps'/>
+//          <Box icon='favorite' color='red' value='120' unit='bpm'/>
+//          <Box icon='wb_sunny' color='yellow' value='-10' unit='˚C'/>
+//           <p>Heart : {heartMin}</p>
+//           <p>Temperature : {tempMin}</p>
+//           <p>Steps : {stepsMin}</p>
+//         </div>
+//       </div>
+//     );
+// }
+// export default App;
 import React from "react";
 import './App.css';
-import { Box} from "./components/Box";
-import './styles/global.css';
+import './styles/global.css'
+import Box from './components/Box';
 
+const tempMin = 20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
 
-
-
-function App() {
-    const tempMin = -20;
-    const tempMax = 40;
-    const heartMin = 80;
-    const heartMax = 180;
-    const stepsMin = 0;
-    const stepsMax = 50000;
-
-    return (
-      <div class = 'container-fluid'>
-        <div class = 'row'>
-         <Box icon='local_drink' color='#3A85FF' value='1.5' unit='L'/>
-         <Box icon='directions_walk' color='black' value='3000' unit='steps'/>
-         <Box icon='favorite' color='red' value='120' unit='bpm'/>
-         <Box icon='wb_sunny' color='yellow' value='-10' unit='˚C'/>
-          <p>Heart : {heartMin}</p>
-          <p>Temperature : {tempMin}</p>
-          <p>Steps : {stepsMin}</p>
-        </div>
-      </div>
-    );
-}
+class App extends React.Component {
+		
+		constructor(props) {
+				super(props);
+        this.state = {
+          water: 0,
+          heart: 120,
+          temperature: -10,
+          steps: 3000
+        }
+		} 
+    calculateWater (){
+      let water = 1.5;
+      if(this.state.temperature > -20){
+        let addTemp = this.state.temperature -20;
+        water+= addTemp * 0.02;
+      } if(this.state.heart > 120){
+      water+= (this.state.heart - 120) * 0.0008;
+    } if (this.state.steps < 1000){
+      water+=(this.state.steps -10000) * 0.00002; 
+    }
+     this.setState({water})
+  }
+    onHeartChange = (e) =>{
+      this.setState ( {
+        heart: e.target.value,
+      
+      });
+      this.calculateWater();
+    }
+    onStepsChange = (e) =>{
+      this.setState ( {
+        steps: e.target.value,
+      
+      });
+      this.calculateWater();
+    }
+    onTemperatureChange = (e) =>{
+      this.setState ( {
+        temperature: e.target.value,
+      
+      }); 
+      this.calculateWater();
+    }
+   
+  
+		render() {
+				return (
+		      <div className="container-fluid">
+		        <div className="row">
+							
+		            {/* Water */}
+		            <Box icon="local_drink" value={this.state.water.toFixed(2)} unit="L" color="#3A85FF" />
+		            
+		            {/* Steps */}
+		            <Box icon="directions_walk" min={stepsMin} max={stepsMax} value={this.state.steps} unit="steps" color="black" onChange={this.onStepsChange} />
+		            
+		            {/* Heart */}
+		            <Box icon="favorite" min={heartMin} max={heartMax} value={this.state.heart} unit="bpm" color="red" onChange={this.onHeartChange} />
+		            
+		            {/* Temperatur */}
+		            <Box icon="wb_sunny" min={tempMin} max={tempMax} value={this.state.temperature} unit="°C" color="yellow" onChange={this.onTemperatureChange} />
+		        </div>
+		      </div>
+		    );
+      }
+    }
 export default App;
--- Chapter-1/elsy/src/components/Box.jsx
+++ Chapter-1/elsy/src/components/Box.jsx
@@ -1,13 +1,43 @@
+// import React from "react";
+
+
+
+// export function Box(props){
+//     return (
+//         <div class = 'box col-sm-3 col-6' background-color = 'white'>
+//             <span className= 'material-icons' style = {{fontSize: 100, color: props.color}}>{props.icon}</span>
+//             <p>{props.value}{props.unit}</p>
+//         </div>
+
+//     );
+// }
 import React from "react";
 
+export default class Box extends React.Component {
+    renderInput() {
+        if(this.props.icon !== 'local_drink'){
+           return (<input type = 'range' min={this.props.min} max={this.props.max} value={this.props.value} onChange={this.props.onChange}/>)
+        } else {
+            return(
+            
+                <div>
+                    
+                </div>
+            )
 
-
-export function Box(props){
-    return (
-        <div class = 'box col-sm-3 col-6' background-color = 'white'>
-            <span className= 'material-icons' style = {{fontSize: 100, color: props.color}}>{props.icon}</span>
-            <p>{props.value}{props.unit}</p>
-        </div>
-
-    );
+        }
+       
+    }
+    render() {
+        return (
+            <div className="box col-sm-3 col-6">
+                <span className="material-icons" style={{fontSize: "100px", color: this.props.color}}>
+                    {this.props.icon}
+                </span>
+                <p>{this.props.value} {this.props.unit}</p>
+                {this.renderInput()}
+            </div>
+        );
+    } 
+  
 }
